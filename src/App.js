6d5ca1a8b207bfb1878116b5ff24fff2f5b32945
Group1 <hongpink473@gmail.com>
2024-11-27 01:54:37 +0700

    React JS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+
+import HomePage from "./pages/HomePage";
+import BillBoard from "./pages/BillBoard";
+import Service from "./pages/Service";
+import Mobile from "./pages/Mobile";
+import SmartWatch from "./pages/SmartWatch";
+import YealySale from "./pages/YealySale";
+import LatestBlog from "./pages/LatestBlog";
+import Testimonials from "./pages/Testimonials";
+import Subscribe from "./pages/Subscribe";
+import Instagram from "./pages/Instagram";
+import Footer from "./pages/Footer";
+import FooterBotton from "./pages/FooterBotton";
+
+import ProductList from "./pages/ProductList";
+import WomenClothing from "./pages/WomenClothing";
+import MenClothing from "./pages/MenClothing";
+import Electronic from "./pages/Electronic";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <BillBoard />
+      <Service />
+      <ProductList />
+      <WomenClothing />
+      <MenClothing />
+      <Electronic />
+      {/* <Mobile /> */}
+      {/* <SmartWatch /> */}
+      <YealySale />
+      <LatestBlog />
+      <Testimonials />
+      <Subscribe />
+      <Instagram />
+      <Footer />
+      <FooterBotton />
+      {/* <Router>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router> */}
     </div>
   );
 }
